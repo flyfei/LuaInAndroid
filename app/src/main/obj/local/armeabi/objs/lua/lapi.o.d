48b90d084745242d88daa246997769e6468789fe
zhaotengfei <zhaotengfei@goyoo.com>
2015-10-30 14:54:15 +0800

    init

---
+++ app/src/main/obj/local/armeabi/objs/lua/lapi.o.d
@@ -0,0 +1,121 @@
+obj/local/armeabi/objs/lua/lapi.o: jni/lua/lapi.c \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/assert.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/math.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ jni/lua/lua.h jni/lua/luaconf.h jni/lua/lapi.h jni/lua/lobject.h \
+ jni/lua/llimits.h jni/lua/ldebug.h jni/lua/lstate.h jni/lua/ltm.h \
+ jni/lua/lzio.h jni/lua/lmem.h jni/lua/ldo.h jni/lua/lfunc.h \
+ jni/lua/lgc.h jni/lua/lstring.h jni/lua/ltable.h jni/lua/lundump.h \
+ jni/lua/lvm.h
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/assert.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/math.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+jni/lua/lua.h:
+
+jni/lua/luaconf.h:
+
+jni/lua/lapi.h:
+
+jni/lua/lobject.h:
+
+jni/lua/llimits.h:
+
+jni/lua/ldebug.h:
+
+jni/lua/lstate.h:
+
+jni/lua/ltm.h:
+
+jni/lua/lzio.h:
+
+jni/lua/lmem.h:
+
+jni/lua/ldo.h:
+
+jni/lua/lfunc.h:
+
+jni/lua/lgc.h:
+
+jni/lua/lstring.h:
+
+jni/lua/ltable.h:
+
+jni/lua/lundump.h:
+
+jni/lua/lvm.h:
---
+++ app/src/main/obj/local/armeabi/objs/luajava/luajava.o.d
@@ -0,0 +1,109 @@
+obj/local/armeabi/objs/luajava/luajava.o: jni/luajava/luajava.c \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/zhaotf/lua/lua-5.1.5/src/lua.h \
+ /Users/zhaotf/lua/lua-5.1.5/src/luaconf.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/zhaotf/lua/lua-5.1.5/src/lualib.h \
+ /Users/zhaotf/lua/lua-5.1.5/src/lua.h \
+ /Users/zhaotf/lua/lua-5.1.5/src/lauxlib.h
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/zhaotf/lua/lua-5.1.5/src/lua.h:
+
+/Users/zhaotf/lua/lua-5.1.5/src/luaconf.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/zhaotf/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/zhaotf/lua/lua-5.1.5/src/lualib.h:
+
+/Users/zhaotf/lua/lua-5.1.5/src/lua.h:
+
+/Users/zhaotf/lua/lua-5.1.5/src/lauxlib.h:
