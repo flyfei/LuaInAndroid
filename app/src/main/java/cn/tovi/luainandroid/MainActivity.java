22a5338897ceb5e6323cf8501972c8ded153be86
zhaotengfei <zhaotengfei9@gmail.com>
2015-11-06 18:50:21 +0800

    add run funcation

--- app/src/main/java/cn/tovi/luainandroid/MainActivity.java
+++ app/src/main/java/cn/tovi/luainandroid/MainActivity.java
@@ -2,12 +2,16 @@ package cn.tovi.luainandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import org.keplerproject.luajava.LuaState;
 import org.keplerproject.luajava.LuaStateFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,8 +41,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mState = (TextView) findViewById(R.id.state);
 
         //init Data
-        nameLists.add("定义一个Lua变量");
-        nameLists.add("run .Lua文件");
+        nameLists.add("Lua变量");
+        nameLists.add("运行 .Lua文件");
 //        nameLists.add("定义一个Lua变量");
         location = 0;
         mState.setText(nameLists.get(location));
@@ -80,7 +84,23 @@ public class MainActivity extends Activity implements View.OnClickListener {
      * 运行Lua脚本文件
      */
     private void runLuaFile() {
+        //加载Lua文件内容
+        mLuaState.LdoString(readRawStream(R.raw.tovi_open_setting));
+        //找到function方法
+        mLuaState.getField(LuaState.LUA_GLOBALSINDEX, "launchSetting");
 
+//        mLuaState.pushJavaObject(getApplicationContext());
+//        mLuaState.pushJavaObject();
+        //传入参数（如果Funcation方法有参数）
+        mLuaState.pushJavaObject(this);
+
+        //执行Function
+        /**
+         * call(int nArgs, int nResults)<br>
+         *     nArgs:几个参数
+         *     nResults:几个返回值?返回值存放的位置？
+         */
+        mLuaState.call(1, 0);
     }
 
     /**
@@ -88,16 +108,70 @@ public class MainActivity extends Activity implements View.OnClickListener {
      */
     private void runStatement() {
 
+        appendResultLine("创建变量Name、取值");
+        //创建Name变量
         mLuaState.LdoString("name = 'Tovi '");
+        //获取值
         mLuaState.getGlobal("name");
-        appendResult("name", mLuaState.toString(-1));
+        //输出值
+        appendResultLine("name", mLuaState.toString(-1));
+        appendResultLine("");//空一行
 
-        mLuaState.LdoString("true");
-        mLuaState.isBoolean(-1);
-        appendResult("'true' is boolean", mLuaState.isBoolean(-1));
+
+//        appendResultLine("创建空变量/赋值/取值");
+//        mLuaState.LdoString("Age");
+//        mLuaState.pushInteger(1);
+//        mLuaState.getGlobal("Age");
+//        appendResultLine("Age", mLuaState.toString(-1));
+//        appendResultLine("");//空一行
+
+        appendResultLine("判断变量属性");
+        //定义变量testType
+        mLuaState.LdoString("testType = true");
+        //获取值
+        mLuaState.getGlobal("testType");
+        //isBoolean 判断值类型
+        appendResultLine("'testType' is boolean", mLuaState.isBoolean(-1));
     }
 
-    private void appendResult(String key, Object o) {
+    private String readRawStream(int resourceId) {
+        InputStream inputStream = null;
+        ByteArrayOutputStream byteArrayOutputStream = null;
+        try {
+            inputStream = getResources().openRawResource(resourceId);
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            int i = inputStream.read();
+            while (i != -1) {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            return byteArrayOutputStream.toString();
+        } catch (Exception e) {
+            Log.e("ReadStream", "读取文件流失败");
+            return "";
+        } finally {
+            if (inputStream != null)
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            if (byteArrayOutputStream != null) {
+                try {
+                    byteArrayOutputStream.flush();
+                    byteArrayOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void appendResultLine(String key, Object o) {
         mResult.append(key + ":" + o + "\n");
+    }
+
+    private void appendResultLine(String s) {
+        mResult.append(s + "\n");
     }
 }
