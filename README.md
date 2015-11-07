44e339f035022239f0040c6c27f24a81996804ae
zhaotengfei <zhaotengfei9@gmail.com>
2015-11-07 16:55:16 +0800

    fix

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 LuaInAndroid
 
-学习Lua在Android上使用，学习项目 https://github.com/mkottman/AndroLua
+[EN](https://github.com/flyfei/LuaInAndroid/blob/master/README_EN.md)
+
+本项目主要为了学习Lua而进行的功能实现的测试
+
+## 测试功能
+
+* Lua变量处理
+* Android（Java）调用Lua Function （或有返回值）
+* Lua 调用 Android（Java）Api
+* Lua 回调调用 Android 的实现方法
+
+
+参考项目 https://github.com/mkottman/AndroLua
--- app/src/main/res/raw/tovi_toast.lua
+++ app/src/main/res/raw/tovi_toast.lua
@@ -9,20 +9,25 @@
 
 -- Lua Toast(直接显示Toast)
 function toast1(context, message, duration)
+    --    获取静态类Toast
     mToast = luajava.bindClass("android.widget.Toast")
-    --    mInstanceToast = luajava.newInstance("android.widget.Toast")
+    --    调用显示Toast Api (Toast.makeText(Context context, CharSequence text, @Duration int duration))
     mToast:makeText(context, message, duration):show()
 end
 
 --Toast的持续时间默认设为LENGTH_SHORT
 function toast2(context, message)
+    --    获取静态类Toast
     mToast = luajava.bindClass("android.widget.Toast")
+    --    如果想获取LENGTH_SHORT,先实例对象,再获取
     mInstanceToast = luajava.newInstance("android.widget.Toast", context)
+    --    调用显示Toast Api (Toast.makeText(Context context, CharSequence text, @Duration int duration))
     mToast:makeText(context, message, mInstanceToast.LENGTH_SHORT):show()
 end
 
 -- Lua Toast(回调调用Android方法显示Toast)
 function toast3(className, context, message, duration)
+    --     调用className里面的toast方法。
     --     被调用的toast(Context context, String message, int duration)必须是Public属性
     className:toast(context, message, duration)
 end
