d7dacb962b0ba314e259c045cb960874cc78de1c
flyfei <zhaotengfei9@gmail.com>
2015-11-10 18:53:06 +0800

    note2

--- app/src/main/java/cn/tovi/luainandroid/ModelTest/Person.java
+++ app/src/main/java/cn/tovi/luainandroid/ModelTest/Person.java
@@ -4,6 +4,10 @@ package cn.tovi.luainandroid.ModelTest;
  * @author <a href='mailto:zhaotengfei9@gmail.com'>Tengfei Zhao</a>
  */
 public class Person {
+    /**
+     * 名字
+     */
     public String name;
+    
     public int age;
 }
